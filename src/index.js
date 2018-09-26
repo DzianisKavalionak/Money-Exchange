b29eead6ebbe12b760720f1f9ac9b087d56e8cba
DzianisKavalionak <nereidy4@gmail.com>
2018-09-26 20:06:14 +0300

    money 1st commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let summ = currency;
+
+    if (summ <= 0) return {};
+    if (summ > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
+    let h = 0;
+    let q = 0;
+    let d = 0;
+    let n = 0;
+    let p = 0;
+    let money = {};
+    function ext(summ) {
+        if (summ >= 50) {
+            h = h + 1;
+            money["H"] = h;
+            return ext(summ - 50);
+        } else {
+            if(summ >= 25) {
+                q = q + 1;
+                money["Q"] = q;
+                return ext(summ - 25);
+            } else {
+                if (summ >= 10) {
+                    d = d + 1;
+                    money["D"] = d;
+                    return ext(summ - 10);
+                } else {
+                    if (summ >= 5) {
+                        n = n + 1;
+                        money["N"] = n;
+                        return ext(summ - 5);
+                    } else { if (summ != 0) {
+                        p = summ;
+                        money["P"] = p;
+                    }
+                    }
+                }
+            }
+        }
+    }
+    ext(summ);
+    return money;
 }
